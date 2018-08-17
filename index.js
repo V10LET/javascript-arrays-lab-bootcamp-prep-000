5a69064057567b8871c308921e7d78606ee78ee5
Violet Moon <violet.wmoon@gmail.com>
2018-08-17 00:30:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function destructivelyPrependKitten(name) {
 
 function destructivelyRemoveLastKitten() {
   kittens.slice(0, kittens.length - 1)
+  return kittens
 }
 
 function appendKitten(name) {
