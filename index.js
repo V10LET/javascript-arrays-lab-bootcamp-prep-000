baab15f6d0d9d3660930621631ffc0831dc70c91
Violet Moon <violet.wmoon@gmail.com>
2018-08-17 00:21:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function kittens() {
   var kittens = ['Milo', 'Otis', 'Garfield']
 }
 
+function appendKitten(name) {
+    return [name, ...kittens]
+}
+
 function destructivelyAppendKitten(name) {
     kittens.appendKitten(name)
     return kittens
